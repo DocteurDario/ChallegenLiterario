4896a0ccad053eb35a1c1ad1098a2e124a226ef0
DocteurDario <docteurdario@gmail.com>
2025-08-10 21:33:25 -0300

    Creando el repo

---
+++ src/main/java/com/aluracursos/challeger_literario/ChallegerLiterarioApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.challeger_literario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallegerLiterarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallegerLiterarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/challeger_literario/ChallegerLiterarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.challeger_literario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallegerLiterarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
