da1f9f4c529d44887a02d0d218a88a7f08515e44
DocteurDario <docteurdario@gmail.com>
2025-08-19 20:04:27 -0300

    Reemplazo contenido del repo

---
+++ src/main/java/com/aluracursos/desafio_literario/DesafioLiterarioApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio_literario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLiterarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLiterarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/desafio_literario/DesafioLiterarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafio_literario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLiterarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
